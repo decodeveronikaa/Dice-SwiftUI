4e7c03c3aeefffe6bab300b8527bce97755c412c
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-26 01:04:33 +0400

    UI changes

--- Dicee-SwiftUI/ContentView.swift
+++ Dicee-SwiftUI/ContentView.swift
@@ -13,11 +13,43 @@ struct ContentView: View {
             Image("background")
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(n: 1)
+                    DiceView(n: 1)
+                    
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                
+            }
         }
     }
 }
+struct DiceView: View {
+    
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
