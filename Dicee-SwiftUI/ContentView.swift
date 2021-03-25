3ea4a05707c204dbc20c09b185f77bead17b7b05
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-25 19:32:52 +0400

    init commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Veronica Markova on 3/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Veronica Markova on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
